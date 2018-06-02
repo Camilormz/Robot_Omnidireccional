838113e1e010723fb0e236095c3f1edd83c4dd5e
Camilormz <38889235+Camilormz@users.noreply.github.com>
2018-06-01 21:11:27 -0400

    Creacion de funciones utiles
    Simplificacion de codigo para las interrupciones y creacion de funciones de movimiento (Avanzar, Rectroceder, Frenar y Liberar)

--- Codigo_Omnidireccional.ino
+++ Codigo_Omnidireccional.ino
@@ -3,6 +3,7 @@
 // Taller de proyecto de robótica y mecatrónica
 
 // Declaración de variables y pines
+// --------------------------------------------------
 // Matriz de pines de control para los motores (La fila determina el numero de motor y la columna el de control), los pines para el control #1 son 30, 33, 35 para los motores 1, 2 y 3
 // respectivamente en el diagrama y los pines para el control #2 son 32, 35 y 34 para los motores 1, 2 y 3 respeticamente el diagrama.
 const byte MotorCtrl[3][2] = {{30,31},{33,32},{35,34}};
@@ -11,7 +12,7 @@ const byte MotorPWM[3] = {2,3,4};
 // Lista de pines interruptores para encoders segun su posición (pines 49, 51 y 53 para encoders 1, 2 y 3 respectivamente en el diagrama)
 const byte Enc[3] = {49,51,53};
 // Lista de interruptores para encoders segun su posición
-const byte Int[3] = {digitalPinToInterrupt(Enc[0]),digitalPinToInterrupt(Enc[1]),digitalPinToInterrupt(Enc[3])};
+const byte Int[3] = {digitalPinToInterrupt(Enc[0]),digitalPinToInterrupt(Enc[1]),digitalPinToInterrupt(Enc[2])};
 
 // Lista de contadores para las interrupciones de los encoders segun su posicion en ella
 volatile byte Contador[3] = {0,0,0};
@@ -22,13 +23,22 @@ volatile unsigned long Tiempo_Fin[3];           // Lista que almacena los tiempo
 volatile float RapidezAngMed[3];                // Lista de rapideces angulares medidas por cada encoder segun su posición en la lista en perforaciones/ms
 volatile float RPMMed[3];                       // Lista de rapideces angulares medidas por cada encoder segun su posicion en la lista en RPM
 
+// Constantes de funcionamiento del robot
+const float MaxRPM = 60;  // Rapidez angular máxima que los motores son capaces de entregar en RPM
+
 // Constantes y variables relacionadas a los encoders e interrupciones
 const byte Nverificador = 5;              // Establece la resolución de medida de la velocidad angular
 const byte TotalPerf = 20;                // Número total de perforaciones en el disco (Perforaciones en un ángulo 2pi)
-const long TiempoParaInterrupcion = 5000; // Tiempo en el cual interrupciones sucesivas de menor tiempo se omitiran en el debouncing en microsegundos.
+const long TiempoParaInterrupcion = 5000; // Tiempo en el cual interrupciones sucesivas de menor tiempo se omitiran en el debouncing en microsegundos
 volatile long UltimoT[3] = {0,0,0};       // Tiempos de control para debouncing para las interrupciones de los encoders segun su posición en la lista
 
-// Creación de funciones útiles
+// Variables de estado para el robot
+float RapidezAngDeseada[3] = {0,0,0}; // Lista que almacena a que rapidez angular que desea que funcione el motor corrspondiente a la posición en la lista en RPM
+byte Avance[3] = {0,0,0};             // Variable que almacena el estado de mov. del motor en la pos. correspondiente (0 a retroceso, 1 a detención (libre o frenada) y 2 a avance)
+bool Frenado[3] = {true,true,true};   // Variable que almacena el estado de frenado del robot para el motor de la posicion correspondietne en la lista
+
+// --------------------------------------------------
+// Creación de funciones útiles para encoders e interrupciones
 // --------------------------------------------------
 // Debounced, función destinada a ejecutarse en una interrupción, retorna true o false dependiendo si esta misma no se ha ejecutado en un tiempo menor a UltT.
 // El objetivo es limpiar la lectura de interrupciones erráticas que ocurren en un tiempo muy pequeño.
@@ -36,7 +46,68 @@ bool Debounced(volatile long UltT){
   long Diferencia = micros() - UltT;
   return Diferencia >= TiempoParaInterrupcion;
 }
-
+// --------------------------------------------------
+// RutinaEncoder, función que ejecuta la accioón que debe realizar cada encoder tomando como parámetro el número de este, cada ISR llama a esta función con el respectivo número del
+// encoder que ejecutó la interrupción.
+void RutinaEncoder(byte NEncoder){
+  byte Num = NEncoder - 1;
+  if (Debounced(UltimoT[Num])){
+    Contador[Num]++;
+    if (Contador[Num] == Nverificador){
+      Tiempo_Fin[Num] = millis();
+      RapidezAngMed[Num] = Contador[Num]/(Tiempo_Fin[Num]-Tiempo_Ini[Num]);
+      Tiempo_Ini[Num] = millis();
+      Contador[Num] = 0;
+    }
+    UltimoT[Num] = micros();
+  }
+}
+// --------------------------------------------------
+// Funciones de movimiento de los motores
+// --------------------------------------------------
+// Liberar, funcion que dado el numero de un motor ejecuta acciones necesarias para que no entregue torque y gire libremente
+void Liberar(byte NMotor){
+  byte indice = NMotor - 1;
+  digitalWrite(MotorCtrl[indice][0],LOW);
+  digitalWrite(MotorCtrl[indice][1],LOW);
+  RapidezAngDeseada[indice] = 0;
+  Avance[indice] = 0;
+  Frenado[indice] = false;
+}
+// --------------------------------------------------
+// Frenar, funcion que dado el numero de un motor ejecuta acciones necesarias para frenarlo
+void Frenar(byte NMotor){
+  byte indice = NMotor - 1;
+  digitalWrite(MotorCtrl[indice][0],HIGH);
+  digitalWrite(MotorCtrl[indice][1],HIGH);
+  RapidezAngDeseada[indice] = 0;
+  Avance[indice] = 0;
+  Frenado[indice] = true;
+}
+// --------------------------------------------------
+// Avanzar, funcion que dado el numero de un motor y una rapidez angular en RPM, ejecuta las acciones necesarias para que avance con las RPM indicadas
+void Avanzar(byte NMotor,float RPM){
+  byte indice = NMotor - 1;
+  float Vel = RPM/MaxRPM;
+  digitalWrite(MotorCtrl[indice][0],HIGH);
+  digitalWrite(MotorCtrl[indice][1],LOW);
+  analogWrite(MotorPWM[indice],Vel);
+  RapidezAngDeseada[indice] = RPM;
+  Avance[indice] = 1;
+  Frenado[indice] = false;
+}
+// --------------------------------------------------
+// Retroceder, funcion que dado el numero de un motor y una rapidez angular en RPM, ejecuta las acciones necesarias para que retroceda con las RPM indicadas
+void Retroceder(byte NMotor,float RPM){
+  byte indice = NMotor - 1;
+  float Vel = RPM/MaxRPM;
+  digitalWrite(MotorCtrl[indice][0],LOW);
+  digitalWrite(MotorCtrl[indice][1],HIGH);
+  analogWrite(MotorPWM[indice],Vel);
+  RapidezAngDeseada[indice] = -RPM;
+  Avance[indice]= 1;
+  Frenado[indice] = false;
+}
 void setup() {
   // Pines de control y PWM
   pinMode(MotorCtrl[0][0],OUTPUT);
@@ -53,45 +124,16 @@ void setup() {
   attachInterrupt(Int[1],Encoder2,RISING);
   attachInterrupt(Int[2],Encoder3,RISING);
 }
-
 void loop() {
   // Rutina a realizar
 }
-
 // Interruptores accionados por encoders
 void Encoder1(){
-  if (Debounced(UltimoT[0])){
-    Contador[0]++;
-    if (Contador[0] == Nverificador){
-      Tiempo_Fin[0] = millis();
-      RapidezAngMed[0] = Contador[0]/(Tiempo_Fin[0]-Tiempo_Ini[0]);
-      Tiempo_Ini[0] = millis();
-      Contador[0] = 0;
-    }
-    UltimoT[0] = micros();
-  }
+  RutinaEncoder(1);
 }
 void Encoder2(){
-  if (Debounced(UltimoT[1])){
-    Contador[1]++;
-    if (Contador[1] == Nverificador){
-      Tiempo_Fin[1] = millis();
-      RapidezAngMed[1] = Contador[1]/(Tiempo_Fin[1]-Tiempo_Ini[1]);
-      Tiempo_Ini[1] = millis();
-      Contador[1] = 0;
-    }
-    UltimoT[1] = micros();
-  }
+  RutinaEncoder(2);
 }
 void Encoder3(){
-  if (Debounced(UltimoT[2])){
-    Contador[2]++;
-    if (Contador[2] == Nverificador){
-      Tiempo_Fin[2] = millis();
-      RapidezAngMed[2] = Contador[2]/(Tiempo_Fin[2]-Tiempo_Ini[2]);
-      Tiempo_Ini[2] = millis();
-      Contador[2] = 0;
-    }
-    UltimoT[2] = micros();
-  }
+  RutinaEncoder(3);
 }
