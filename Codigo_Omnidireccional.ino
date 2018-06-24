3504f545d05ec8cb40f8854666c8e382dfbb6542
Camilormz <38889235+Camilormz@users.noreply.github.com>
2018-06-24 02:03:35 -0400

    Adición de movimientos globales (Alpha v1.0)
    Termino de implementación de respuestas a comandos.
    
    - Eliminación de las funciones realacionadas al PID e implementación de una lista de estas
    - Creación de funciones de movimiento globales.
    
    Queda pendiente:
    - Testear código con robot ensamblado
    - Ajustar constantes  Kp, Ki y Kd
    - Implementar recepción de instrucciones

--- Codigo_Omnidireccional.ino
+++ Codigo_Omnidireccional.ino
@@ -1,10 +1,26 @@
 // Código para robot omnidireccional
 // Grupo 6 ; EI2001-5 ; Otoño 2018 ; FCFM ; U. de Chile
 // Taller de proyecto de robótica y mecatrónica
+// --------------------------------------------------
+// Nota: El origen del robot estará en su centro de masa (ortocentro del triangulo truncado del armazón), el eje y apunta hacia la rueda 1 (dónde se ubica el motor 1), el eje x es
+// perpendicular al y hacia la derecha (formando un plano cartesiano), los ángulos se miden en sentido antihorario, el sistema descrito es solidario al movimiento del robot.
+// Todo el programa se basa en el sistema mencionado.
+// --------------------------------------------------
 
 #include <PID_v1.h> // Librería de PID
+#include <math.h>   // Librería de funciones matemáticas
 
-// Declaración de variables y pines
+// --------------------------------------------------
+// Funciones de conversión de unidades
+// --------------------------------------------------
+// GradosARad, función que dado un ángulo en grados sexagesimales, lo convierte a radianes
+double GradosARad(double Grados){return PI*Grados/180;}
+// RadsARPM, función que dada una rapidez angular en rad/s, la convierte a RPM
+double RadsARPM(double rads){return 30*rads/PI;}
+// GradsARads, función que dada una rapidez angular en grados/s, la convierte a rad/s
+double GradsARads(double grads){return PI*grads/180;}
+// --------------------------------------------------
+// Declaración de variables, pines y constantes
 // --------------------------------------------------
 // Matriz de pines de control para los motores (La fila determina el numero de motor y la columna el de control), los pines para el control #1 son 30, 33, 35 para los motores 1, 2 y 3
 // respectivamente en el diagrama y los pines para el control #2 son 32, 35 y 34 para los motores 1, 2 y 3 respeticamente el diagrama.
@@ -26,8 +42,12 @@ volatile float RapidezAngMed[3];                // Lista de rapideces angulares 
 volatile float RPMMed[3];                       // Lista de rapideces angulares medidas por cada encoder segun su posicion en la lista en RPM
 double RapidezInPID[3] = {0,0,0};               // Lista de rapideces angulares medidas ajustadas al rango PWM requerido por los motores
 
-// Constantes de funcionamiento del robot
-const float MaxRPM = 60;  // Rapidez angular máxima que los motores son capaces de entregar en RPM
+// Constantes de funcionamiento y especificaciones del robot
+const float MaxRPM = 60;                  // Rapidez angular máxima que los motores son capaces de entregar en RPM
+const double RadioRuedas = 50;            // Radio de las ruedas en mm
+const double DistanciaAlCM = 250;         // Distancia desde el plano de giro de la rueda al centro de masas del robot en mm
+const double AngRuedas[3] = {0,120,240};  // Lista que almacena el angulo que forma el eje centro de masas - rueda con respecto al eje y en grados
+const double AngRadRuedas[3] = {GradosARad(AngRuedas[0]),GradosARad(AngRuedas[1]),GradosARad(AngRuedas[2])}; // Lista que almacena lo mismo que AngRuedas pero en radianes
 
 // Constantes y variables relacionadas a los encoders e interrupciones
 const byte Nverificador = 5;              // Establece la resolución de medida de la velocidad angular
@@ -36,10 +56,10 @@ const long TiempoParaInterrupcion = 5000; // Tiempo en el cual interrupciones su
 volatile long UltimoT[3] = {0,0,0};       // Tiempos de control para debouncing para las interrupciones de los encoders segun su posición en la lista
 
 // Variables de estado para el robot
-float RapidezAngDeseada[3] = {0,0,0}; // Lista que almacena a que rapidez angular que desea que funcione el motor corrspondiente a la posición en la lista en RPM
-double RapidezSetpointPID[3];         // Lista que almacena los setpoints de rapidez angular para cada motor en el rango PWM correspondiente
-byte Avance[3] = {0,0,0};             // Variable que almacena el estado de mov. del motor en la pos. correspondiente (0 a retroceso, 1 a detención (libre o frenada) y 2 a avance)
-bool Frenado[3] = {true,true,true};   // Variable que almacena el estado de frenado del robot para el motor de la posicion correspondietne en la lista
+double RapidezAngDeseada[3] = {0,0,0};  // Lista que almacena a que rapidez angular que desea que funcione el motor corrspondiente a la posición en la lista en RPM
+double RapidezSetpointPID[3];           // Lista que almacena los setpoints de rapidez angular para cada motor en el rango PWM correspondiente
+byte Avance[3] = {0,0,0};               // Variable que almacena el estado de mov. del motor en la pos. correspondiente (0 a retroceso, 1 a detención (libre o frenada) y 2 a avance)
+bool Frenado[3] = {true,true,true};     // Variable que almacena el estado de frenado del robot para el motor de la posicion correspondietne en la lista
 
 // Declaraciones y variables para control PID
 double Kp = 1;                // Constante proporcional de los PID
@@ -47,12 +67,16 @@ double Ki = 1;                // Constante integral de los PID
 double Kd = 1;                // Constante derivativa de los PID
 double RapidezOutPID[3];      // Lista de rapideces angulares de salida post control PID ajustadas al rango PWM requerido por los motores
 int TiempoDeMuestreo = 200;   // Tiempo de computo del PID en ms
-PID PID1(&RapidezInPID[0],&RapidezOutPID[0],&RapidezSetpointPID[0],Kp,Ki,Kd,DIRECT,P_ON_M);
-PID PID2(&RapidezInPID[1],&RapidezOutPID[1],&RapidezSetpointPID[1],Kp,Ki,Kd,DIRECT,P_ON_M);
-PID PID3(&RapidezInPID[2],&RapidezOutPID[2],&RapidezSetpointPID[2],Kp,Ki,Kd,DIRECT,P_ON_M);
+PID PIDs[3] = {PID(&RapidezInPID[0],&RapidezOutPID[0],&RapidezSetpointPID[0],Kp,Ki,Kd,DIRECT,P_ON_M),   // Lista de PIDs, donde cada cual controla la rapidez angular del motor
+               PID(&RapidezInPID[1],&RapidezOutPID[1],&RapidezSetpointPID[1],Kp,Ki,Kd,DIRECT,P_ON_M),   // correspondiente a su posición en la lista
+               PID(&RapidezInPID[2],&RapidezOutPID[2],&RapidezSetpointPID[2],Kp,Ki,Kd,DIRECT,P_ON_M)};
+
+// Variables de trabajo para funciones asociadas al movimiento completo del robot
+double RPMMov[3];     // Lista que almacena los valores trabajados por MovimientoXYRot(), ver función para mas detalles
+double RPMLimMov[3];  // Lista que almacena los valores trabajados por LimitarMov(), ver función para mas detalles
 
 // --------------------------------------------------
-// Creación de funciones útiles para encoders e interrupciones
+// Funciones útiles para encoders e interrupciones
 // --------------------------------------------------
 // Debounced, función destinada a ejecutarse en una interrupción, retorna true o false dependiendo si esta misma no se ha ejecutado en un tiempo menor a UltT.
 // El objetivo es limpiar la lectura de interrupciones erráticas que ocurren en un tiempo muy pequeño.
@@ -77,38 +101,9 @@ void RutinaEncoder(byte NEncoder){
     }
     UltimoT[Num] = micros();
   }
-  ComputarPID(NEncoder);
+  PIDs[Num].Compute();
   if (Avance[Num] != 1) {
     analogWrite(MotorPWM[Num],byte(RapidezOutPID[Num]));
-  }
-}
-// --------------------------------------------------
-// Funciones útiles para manejo de PID
-// --------------------------------------------------
-// ApagarPID, función que dado el número de un PID, lo apaga
-void ApagarPID(byte NPID){
-  switch (NPID){
-    case 1: PID1.SetMode(MANUAL); break;
-    case 2: PID2.SetMode(MANUAL); break;
-    case 3: PID3.SetMode(MANUAL); break;
-  }
-}
-// --------------------------------------------------
-// EncenderPID, funcion que dado el número de un PID, lo enciende
-void EncenderPID(byte NPID){
-  switch (NPID){
-    case 1: PID1.SetMode(AUTOMATIC); break;
-    case 2: PID2.SetMode(AUTOMATIC); break;
-    case 3: PID3.SetMode(AUTOMATIC); break;
-  }
-}
-// --------------------------------------------------
-// ComputarPID, funcion que computa el PID asociado al número dado
-void ComputarPID(byte NPID){
-  switch (NPID){
-    case 1: PID1.Compute(); break;
-    case 2: PID2.Compute(); break;
-    case 3: PID3.Compute(); break;
   }
 }
 // --------------------------------------------------
@@ -117,7 +112,7 @@ void ComputarPID(byte NPID){
 // Liberar, funcion que dado el numero de un motor ejecuta acciones necesarias para que no entregue torque y gire libremente
 void Liberar(byte NMotor){
   byte indice = NMotor - 1;
-  ApagarPID(NMotor);
+  PIDs[indice].SetMode(MANUAL);
   digitalWrite(MotorCtrl[indice][0],LOW);
   digitalWrite(MotorCtrl[indice][1],LOW);
   RapidezAngDeseada[indice] = 0;
@@ -129,7 +124,7 @@ void Liberar(byte NMotor){
 // Frenar, funcion que dado el numero de un motor ejecuta acciones necesarias para frenarlo
 void Frenar(byte NMotor){
   byte indice = NMotor - 1;
-  ApagarPID(NMotor);
+  PIDs[indice].SetMode(MANUAL);
   digitalWrite(MotorCtrl[indice][0],HIGH);
   digitalWrite(MotorCtrl[indice][1],HIGH);
   RapidezAngDeseada[indice] = 0;
@@ -139,32 +134,111 @@ void Frenar(byte NMotor){
 }
 // --------------------------------------------------
 // Avanzar, funcion que dado el numero de un motor y una rapidez angular en RPM, ejecuta las acciones necesarias para que avance con las RPM indicadas
-void Avanzar(byte NMotor,float RPM){
+void Avanzar(byte NMotor,double RPM){
   byte indice = NMotor - 1;
-  EncenderPID(NMotor);
+  PIDs[indice].SetMode(AUTOMATIC);
   RapidezAngDeseada[indice] = RPM;
   RapidezSetpointPID[indice] = 255*RPM/MaxRPM;
   digitalWrite(MotorCtrl[indice][0],HIGH);
   digitalWrite(MotorCtrl[indice][1],LOW);
   Avance[indice] = 2;
   Frenado[indice] = false;
-  ComputarPID(NMotor);
+  PIDs[indice].Compute();
   analogWrite(MotorPWM[indice],byte(RapidezOutPID[indice]));
 }
 // --------------------------------------------------
 // Retroceder, funcion que dado el numero de un motor y una rapidez angular en RPM, ejecuta las acciones necesarias para que retroceda con las RPM indicadas
-void Retroceder(byte NMotor,float RPM){
+void Retroceder(byte NMotor,double RPM){
   byte indice = NMotor - 1;
-  EncenderPID(NMotor);
+  PIDs[indice].SetMode(AUTOMATIC);
   RapidezAngDeseada[indice] = -RPM;
   RapidezSetpointPID[indice] = 255*RPM/MaxRPM;
   digitalWrite(MotorCtrl[indice][0],LOW);
   digitalWrite(MotorCtrl[indice][1],HIGH);
   Avance[indice]= 0;
   Frenado[indice] = false;
-  ComputarPID(NMotor);
+  PIDs[indice].Compute();
   analogWrite(MotorPWM[indice],byte(RapidezOutPID[indice]));
 }
+// --------------------------------------------------
+// Funciones para movimiento global del robot
+// --------------------------------------------------
+// MovimientoXYRot, función que dada una velocidad en el eje x en mm/s, una en el eje y en mm/s y una rapidez angular en grados/s, modifica la lista RPMMov, con los valores
+// correspondientes a las RPM que debe ejecutar cada motor según su posición en la lista para realizar el movimiento descrito
+void MovimientoXYRot(double Vx,double Vy,double Rot){
+  double theta;
+  if(Vy!=0){theta = atan2(Vx,Vy);}
+  else if(Vx>0){theta = PI/2;}
+  else{theta = -PI/2;}
+  double RotRads = GradsARads(Rot);
+  double AngParaCal1 = theta + AngRadRuedas[0];
+  double AngParaCal2 = theta + AngRadRuedas[1];
+  double AngParaCal3 = theta + AngRadRuedas[2];
+  double RapAng1_p1 = (cos(theta)*cos(AngParaCal1)-sin(theta)*sin(AngParaCal1))*Vx;
+  double RapAng2_p1 = (cos(theta)*cos(AngParaCal2)-sin(theta)*sin(AngParaCal2))*Vx;
+  double RapAng3_p1 = (cos(theta)*cos(AngParaCal3)-sin(theta)*sin(AngParaCal3))*Vx;
+  double RapAng1_p2 = -(sin(theta)*cos(AngParaCal1)+cos(theta)*sin(AngParaCal1))*Vy;
+  double RapAng2_p2 = -(sin(theta)*cos(AngParaCal2)+cos(theta)*sin(AngParaCal2))*Vy;
+  double RapAng3_p2 = -(sin(theta)*cos(AngParaCal3)+cos(theta)*sin(AngParaCal3))*Vy;
+  double RapAng1_p3 = DistanciaAlCM*RotRads;
+  double RapAng2_p3 = DistanciaAlCM*RotRads;
+  double RapAng3_p3 = DistanciaAlCM*RotRads;
+  double RapAng1 = (RapAng1_p1 + RapAng1_p2 + RapAng1_p3)/RadioRuedas;
+  double RapAng2 = (RapAng2_p1 + RapAng2_p2 + RapAng2_p3)/RadioRuedas;
+  double RapAng3 = (RapAng3_p1 + RapAng3_p2 + RapAng3_p3)/RadioRuedas;
+  double RPM1 = RadsARPM(RapAng1);
+  double RPM2 = RadsARPM(RapAng2);
+  double RPM3 = RadsARPM(RapAng3);
+  RPMMov[0] = RPM1;
+  RPMMov[1] = RPM2;
+  RPMMov[2] = RPM3;
+}
+// --------------------------------------------------
+// LimitarMov, funcion que dados 3 valores correspondientes a RPM que debieran tener los motores para mover el robot de cierta forma, revisa que no sobrepasen las RPM máximas del mismo
+// motor, en caso de que lo hagan, disminuye las RPM de todos en igual medida, los valores para cada motor, se guardan en la posición correspondiente de la lista RPMLimMov, retora true
+// en caso de modificar los valores, false en caso contrario.
+bool LimitarMov(double RPM1,double RPM2,double RPM3){
+  double MaxPar = max(abs(RPM1),abs(RPM2));
+  double Max = max(MaxPar,abs(RPM3));
+  if(Max > MaxRPM){
+    double factor = MaxRPM/Max;
+    RPMLimMov[0] = RPM1*factor;
+    RPMLimMov[1] = RPM2*factor;
+    RPMLimMov[2] = RPM3*factor;
+    return true;
+  }
+  else{
+    RPMLimMov[0] = RPM1;
+    RPMLimMov[1] = RPM2;
+    RPMLimMov[2] = RPM3;
+    return false;
+  }
+}
+// --------------------------------------------------
+// MoverXYRotRobot, funcion que ejecuta las ordenes necesarias para mover el robot con parámetros análogos a la función MovimientoXYRot
+void MoverXYRotRobot(double Vx,double Vy,double Rot){
+  MovimientoXYRot(Vx,Vy,Rot);
+  LimitarMov(RPMMov[0],RPMMov[1],RPMMov[2]);
+  for(byte i=0;i<3;i++){
+    if     (RPMMov[i] < 0) {Retroceder(i+1,-RPMMov[i]);}
+    else if(RPMMov[i] == 0){Liberar(i+1);}
+    else                   {Avanzar(i+1,RPMMov[i]);}
+  }
+}
+// --------------------------------------------------
+// FrenarRobot, funcion que ejecuta las ordenes necesarias para frenar el robot
+void FrenarRobot(){Frenar(1);Frenar(2);Frenar(3);}
+// --------------------------------------------------
+// MoverPolarRotRobot, funcion que ejecuta las ordenes necesarias para mover el robot usando un sistema polar, dónde los ángulos se miden desde del eje y cartesiano, el primer parámetro
+// es la rapidez resultante, el segundo el ángulo y el tercero la rotación (Análoga a MoverXYRotRobot())
+void MoverPolarRotRobot(double Rapidez,double Angulo,double Rot){
+  double VelX = -Rapidez*sin(Angulo);
+  double VelY = Rapidez*cos(Angulo);
+  MoverXYRotRobot(VelX,VelY,Rot);
+}
+// --------------------------------------------------
+// Funciones principales de iniciación y ciclo de Arduino
+// --------------------------------------------------
 void setup() {
   // Pines de control y PWM
   pinMode(MotorCtrl[0][0],OUTPUT);
@@ -180,31 +254,17 @@ void setup() {
   attachInterrupt(Int[0],Encoder1,RISING);
   attachInterrupt(Int[1],Encoder2,RISING);
   attachInterrupt(Int[2],Encoder3,RISING);
-  PID1.SetSampleTime(TiempoDeMuestreo);
-  PID2.SetSampleTime(TiempoDeMuestreo);
-  PID3.SetSampleTime(TiempoDeMuestreo);
+  // Ajuste de PIDs
+  PIDs[0].SetSampleTime(TiempoDeMuestreo);
+  PIDs[1].SetSampleTime(TiempoDeMuestreo);
+  PIDs[2].SetSampleTime(TiempoDeMuestreo);
 }
 void loop() {
-  /* Rutina a realizar
-  Retroceder(1,60);
-  Avanzar(2,55);
-  Frenar(3);
-  delay(5000);
-  Retroceder(2,20);
-  Avanzar(3,50);
-  Frenar(1);
-  delay(5000);
-  Retroceder(3,60);
-  Avanzar(1,50);
-  Frenar(2);
-  delay(5000);
-  Liberar(1);
-  Liberar(2);
-  Liberar(3);
-  delay(5000);
-  */
+  // Vacío, depende del método de entrada de instrucciones
 }
+// --------------------------------------------------
 // Interruptores accionados por encoders
+// --------------------------------------------------
 void Encoder1(){
   RutinaEncoder(1);
 }
